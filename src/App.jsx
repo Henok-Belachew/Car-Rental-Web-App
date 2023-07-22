1004dcccfcde44d19bc835752db5b6ad72865740
Henok Belachew <henokbelachewstu@gmail.com>
2023-07-22 15:42:19 +0300

    Project starter files and packages needed insatlled!

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function App() {
+  return (
+    <div>
+      <h1 className='bg-red-700'>
+        React is working fine!
+      </h1>
+    </div>
+  )
+}
+
+export default App
---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,kk,jsx}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
