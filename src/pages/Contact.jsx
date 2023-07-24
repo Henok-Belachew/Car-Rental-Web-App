37828b808e914705ced4735dc6eeed11cffcae5a
Henok Belachew <henokbelachewstu@gmail.com>
2023-07-25 00:13:39 +0300

    ALMOST DONE!

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,9 +1,23 @@
 import React from 'react'
+import HeroCar from '../assets/HeroImages/HeroTopImg.png'
+import WhatsApp from '../components/WhatsApp'
 
 function Contact() {
   return (
-    <div>
-      <h1>Contact</h1>
+    <div className='relative z-[4]' >
+      <div className='relative top-[-100px] z-[4] h-[410px] w-[100vw]'>
+      <img src={HeroCar} className='h-full object-cover w-full relative z-[2]' alt="" />
+      <h1 className='absolute z-[5] top-0 flex flex-col max-lg:text-center justify-center px-[67px] text-black font-[700] text-[38px]  h-full w-full'>
+        
+        Contact
+        <span className='font-[400] text-[16px]'>Home/Contact</span>
+      
+      </h1>
+      <h1 className='absolute z-[3] top-0 bg-white opacity-[90%] text-black font-[700] text-[48px] text-center h-full w-full'></h1>
+    
+    </div>
+
+    <WhatsApp/>
     </div>
   )
 }
